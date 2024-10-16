8ac6f6e9daaaa9f4c73bd4420c1760ca4fcc349a
LuisRamosz <luisfelipesr48@gmail.com>
2024-10-15 21:31:27 -0300

    tde

---
+++ Bicicleta.cs
@@ -0,0 +1,18 @@
+public class Bicicleta : Veiculo, IAceleravel
+{
+    public Bicicleta(string marca, string modelo)
+    :base(marca, modelo)
+    {
+
+    }
+
+    public void Acelerar()
+    {
+        Console.WriteLine($"A {Marca}, {Modelo} está pedalando mais rápido");
+    }
+
+    public override void Ligar()
+    {
+        Console.WriteLine($"{Marca}, {Modelo} está pronto para uso");
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,18 @@
+public abstract class Veiculo
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    
+    public Veiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    public abstract void Ligar();
+
+    public void Desligado()
+    {
+        Console.WriteLine("O veiculo está desligado");
+    }
+}
